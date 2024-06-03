c6e3fa4d19e2e33016cdaaf0a3095cafedb5fc0c
mat <matthieu.muratore@gmail.com>
2024-06-03 08:40:37 +0200

    refactor: rename test project

--- test/Weather.Forecast.Domain.Test/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Weather.Forecast.Domain.Test;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
--- test/Weather.Forecast.Integration.Test/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace Weather.Forecast.Integration.Test;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
