88ca8ab5f7fff1d2de9caeb04876d756c14dae90
Matthieu Muratore <mmu@eonix.be>
2024-06-30 09:29:40 +0200

    chore: remove HealthChecker.csproj

--- .local/.healthchecker/HealthChecker/Program.cs
+++
@@ -1,7 +0,0 @@
-var httpClient = new HttpClient();
-httpClient.DefaultRequestHeaders.ConnectionClose = true;
-
-if (args.Length > 1 && Uri.TryCreate(args[1], UriKind.Absolute, out var uri))
-    return await httpClient.GetAsync(uri).ContinueWith(r => r.Result.IsSuccessStatusCode) ? 0 : 1;
-
-throw new ArgumentException("A valid URI must be given as first argument");
