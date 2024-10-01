6e6f42ba7ea28628323af439063e4f248f612290
mat <matthieu.muratore@gmail.com>
2024-10-01 10:19:05 +0200

    feat: add request info in case of http error

--- src/Weather.Api/Configuration/Observability/HttpContextProcessor.cs
+++ src/Weather.Api/Configuration/Observability/HttpContextProcessor.cs
@@ -1,4 +1,6 @@
 ﻿using System.Diagnostics;
+using System.Net;
+using System.Text.Json;
 using OpenTelemetry;
 
 namespace Weather.Api.Configuration.Observability;
@@ -7,9 +9,32 @@ internal sealed class HttpContextProcessor(IHttpContextAccessor httpContextAcces
 {
     public override void OnStart(Activity data)
     {
-        var username = httpContextAccessor.HttpContext?.User.Identity?.Name;
-        if (username is not null) data.SetTag(nameof(username), username);
+        SetUsernameTag(data);
 
         base.OnStart(data);
+    }
+
+    private void SetUsernameTag(Activity data)
+    {
+        var username = httpContextAccessor.HttpContext?.User.Identity?.Name;
+        if (username is not null) data.SetTag(nameof(username), username);
+    }
+
+    public override void OnEnd(Activity data)
+    {
+        SetTagsInCaseOfHttpError(data);
+
+        base.OnEnd(data);
+    }
+
+    private void SetTagsInCaseOfHttpError(Activity data)
+    {
+        var response = httpContextAccessor.HttpContext?.Response;
+        if (response is null) return;
+
+        if (response.StatusCode < (int)HttpStatusCode.BadRequest) return;
+        
+        data.SetTag(nameof(HttpRequest), JsonSerializer.Serialize(httpContextAccessor.HttpContext?.Request));
+        data.SetTag(nameof(HttpResponse), JsonSerializer.Serialize(response));
     }
 }
